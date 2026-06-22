d2d1677285601a492a80f1f279a2a9076d6dd412
Tan5dev <tanmaypathe4@gmail.com>
2026-06-22 22:32:46 +0530

    Create 02_Data_Types/data_types.py

---
+++ 02_Data_Types/data_types.py
@@ -0,0 +1,30 @@
+"""
+=========================================
+Python Basics - Data Types
+Author: Tanmay Pathe
+=========================================
+"""
+
+name = "Tanmay"
+age = 18
+height = 5.9
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+fruits = ["Apple", "Banana", "Mango"]
+colors = ("Red", "Green", "Blue")
+numbers = {1, 2, 3, 4}
+
+student = {
+    "name": "Tanmay",
+    "age": 18
+}
+
+print(type(fruits))
+print(type(colors))
+print(type(numbers))
+print(type(student))

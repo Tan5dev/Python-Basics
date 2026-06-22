0839a9c7d318d6b517e95f7f92b876e3e2295cbe
Tan5dev <tanmaypathe4@gmail.com>
2026-06-22 22:17:14 +0530

    Create Variables.py

---
+++ 01_Variables/Variables.py
@@ -0,0 +1,45 @@
+"""
+=========================================
+Python Basics - Variables
+Author: Tanmay Pathe
+=========================================
+"""
+
+# Creating variables
+
+name = "Tanmay"
+age = 18
+height = 5.9
+is_student = True
+
+# Printing variables
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Multiple assignments
+
+x, y, z = 10, 20, 30
+
+print("\nMultiple Assignment:")
+print(x, y, z)
+
+# Assigning same value
+
+a = b = c = 100
+
+print("\nSame Value Assignment:")
+print(a, b, c)
+
+# Variable naming examples
+
+first_name = "Tanmay"   # Valid
+_last_name = "Pathe"    # Valid
+age1 = 18               # Valid
+
+print("\nVariable Naming:")
+print(first_name)
+print(_last_name)
+print(age1)

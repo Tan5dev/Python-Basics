93fae78add5315674860c857c1ae996e86f08313
Tan5dev <tanmaypathe4@gmail.com>
2026-06-23 07:00:47 +0530

    Create 03_Input_Output/ input_output.py

---
+++ 03_Input_Output/ input_output.py
@@ -0,0 +1,26 @@
+"""
+=========================================
+Python Basics - Input & Output
+Author: Tanmay Pathe
+=========================================
+"""
+
+# Output
+
+print("Welcome to Python Basics!")
+
+# Input
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Formatted Output
+
+print(f"\nHello, {name}!")
+print(f"You are {age} years old.")
+
+# Escape Characters
+
+print("\nEscape Character Example:")
+print("Python\nProgramming")
+print("Python\tProgramming")
